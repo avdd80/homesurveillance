b506a949c27c14febfcfe9bd8d6e394d18d31e1e
Abhijeet Deshpande <avdd80@gmail.com>
2015-09-19 03:53:33 -0700

    Add start cam script 1

---
+++ client/scripts/start_cam_320x240_5_night.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/usr/src/mjpg-streamer/mjpg-streamer-experimental
+/usr/src/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -p 8080" -i "input_raspicam.so -x 320 -y 240 -fps 5 -ex night"
