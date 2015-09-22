82ec9e9fc0d5b3f29125863f6af14f9bcaa2e79b
Abhijeet Deshpande <avdd80@gmail.com>
2015-09-22 00:03:31 -0700

    more logs

--- server/job_scheduler.py
+++ server/job_scheduler.py
@@ -37,7 +37,9 @@ class Sched_Obj:
 	self.__cam   = Cam_Object ()
         log.print_high ('Created camera object')
 
+        log.print_high ('Before Blocking scheduler obj')
         self.__sched = BlockingScheduler()
+        log.print_high ('Blocking scheduler obj created')
         self.__sched.start()        # start the scheduler
         log.print_high ('Scheduler started')
 
