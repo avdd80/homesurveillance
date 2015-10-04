7fed29ccef6847c6b8797b8da05b24719f9bde47
Abhijeet Deshpande <avdd80@gmail.com>
2015-10-04 01:58:40 -0700

    Add code to copy autostart scripts to .config/autostart folder

--- setup.py
+++ setup.py
@@ -16,6 +16,15 @@ system ('sudo chmod +x client/display_stream_launcher.py')
 system ('sudo chmod +x client/tcp_monitor.py')
 system ('sudo chmod +x client/early_server_monitor.py')
 
+# Make autostart scripts executable
+system ('sudo chmod +x scripts/instapush_notif.desktop')
+system ('sudo chmod +x scripts/main.desktop')
+
+system ('sudo mkdir                              /home/pi/.config/autostart')
+system ('sudo mv scripts/instapush_notif.desktop /home/pi/.config/autostart/.')
+system ('sudo mv scripts/main.desktop            /home/pi/.config/autostart/.')
+
+
 # Set the correct timezone
 system ('sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime')
 
